0d43af1d67a5cdc5c5247e3962581e1c47ff0161
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-20 09:41:46 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+
 
 
 class App extends Component {
@@ -13,6 +15,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu />
       </div>
     );
   }
